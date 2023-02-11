37b57c3176747bbaf6fcba53381627c845903cf2
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-02-11 14:01:10 +0000

    setup recap

---
+++ ruby_playground.rb
@@ -0,0 +1,25 @@
+my_array = ["back-end", "front-end", "full-stack", "low-code"]
+
+# 1.1 - Print element "full-stack" on the terminal
+
+
+# 1.2 - Print every element from my_array
+
+
+# 1.3 - Print every element with associated index
+
+
+
+gabriel = {
+  name: "Gabriel",
+  age: 35,
+  profession: "full_stack_developer",
+  salary: 1000
+}
+
+# 2.1 Print the value of the name and age on the terminal
+
+
+# 2.2 Give me a payrise
+
+
