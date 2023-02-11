6b73b75d37a1759743d1bbc21f3c22016cb9e9a8
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-02-11 19:39:19 +0000

    Recap final version

--- ruby_playground.rb
+++ ruby_playground.rb
@@ -23,9 +23,24 @@ gabriel = {
 # 2.2 Give me a payrise
 
 
+# 2.3 Add key nacionality: brazilian 
 
-shopping_cart = {
-  banana: 1,
-  apple: 50,
-  
-}
+
+# 3. Create a class Person with attributes name, age, profession and salary:
+
+
+# 4. Instantiate that person with specific attributes:
+
+
+
+
+
+
+# Games challenge - Easy like a sunday morning! =)
+#     Create table games with name, genre & address (inherit from ActiveRecord::Migration[7.0])
+#     Create model (inherit from ActiveRecord::Base)
+#     Generate 5 games as seeds (use faker if you want)
+#     Understand router to check what we need to implement
+#     Start action by action (router > controller > view )
+#     Run lib/app.rb to test it.
+#     Have fun
